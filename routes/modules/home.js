1ef915f377f1b76b7ebd2a3b5629f7ea796cff52
maximeri <t.s.eliot1996@gmail.com>
2021-12-30 19:02:54 +0800

    feat: add sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -9,4 +9,32 @@ router.get('/', (req, res) => {
     .then(restaurants => res.render('index', { restaurants: restaurants }))
 })
 
+router.get('/sort-name-asc',(req,res)=>{
+  restaurants.find()
+  .lean()
+  .sort({ name: 'asc' })
+  .then(restaurants => res.render('index', { restaurants: restaurants }))
+})
+
+router.get('/sort-name-desc', (req, res) => {
+    restaurants.find()
+      .lean()
+      .sort({ name: 'desc' })
+      .then(restaurants => res.render('index', { restaurants: restaurants }))
+})
+
+router.get('/sort-category', (req, res) => {
+    restaurants.find()
+      .lean()
+      .sort({ category: 'asc' })
+      .then(restaurants => res.render('index', { restaurants: restaurants }))
+})
+
+router.get('/sort-location', (req, res) => {
+    restaurants.find()
+      .lean()
+      .sort({ location: 'asc' })
+      .then(restaurants => res.render('index', { restaurants: restaurants }))
+})
+
 module.exports = router
