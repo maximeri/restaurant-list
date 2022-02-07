91c10060ff23fa81eb756ad554e18cca4a818840
maximeri <t.s.eliot1996@gmail.com>
2022-02-07 12:33:29 +0800

    feat: add user model

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const restaurantsSchema = new Schema({
+const restaurantSchema = new Schema({
   name: {
     type: String,
     reauired: true
@@ -39,4 +39,6 @@ const restaurantsSchema = new Schema({
   }
 })
 
-module.exports = mongoose.model('restaurants', restaurantsSchema)
+module.exports = mongoose.model('Restaurant', restaurantSchema) // export 我 create 的 mongoose 的 model(包含 method）的實例（instance） 叫 restaurants  
+
+// mongoose 原文：Models are fancy constructors compiled from Schema definitions. An instance of a model is called a document. Models are responsible for creating and reading documents from the underlying MongoDB database.
---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const userSchema = new Schema({
+  name: {
+    type: String,
+    reauired: false
+  },
+  email: {
+    type: String,
+    reauired: true
+  },
+  password: {
+    type: String,
+    reauired: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('User', userSchema) // export 我 create 的 mongoose 的 model(包含 method）的實例（instance） 叫 restaurants  
+
+// mongoose 原文：Models are fancy constructors compiled from Schema definitions. An instance of a model is called a document. Models are responsible for creating and reading documents from the underlying MongoDB database.
