1d01d8cd6f6e555302eb6bd857a49730d453f957
maximeri <t.s.eliot1996@gmail.com>
2022-02-10 20:15:36 +0800

    feat: add isAuthenticated to display different view

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,20 @@
+// module.exports = {
+//   authenticator: (req, res, next) => {
+//   if (req.isAuthenticated()) {
+//     //req.isAuthenticated() will return true if user is logged in
+//     return next();  // 接下來去 home
+//   } else {
+//     res.redirect('/users/login');
+//   }
+// }
+// }
+
+
 module.exports = {
   authenticator: (req, res, next) => {
-  if (req.isAuthenticated()) {
-    //req.isAuthenticated() will return true if user is logged in
-    next();  // 接下來去 home
-  } else {
-    res.redirect("/users/login");
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/users/login')
   }
-}
 }
