14f54a2f7f4b138f087dd74c0c6c73a1c6e03c3f
maximeri <t.s.eliot1996@gmail.com>
2022-02-12 15:41:43 +0800

    feat:add bcrypt.js

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 const User = require('../../models/user')
 
@@ -56,10 +57,14 @@ router.post('/register', (req,res)=>{
           confirmPassword
         })
       } else {
-        return User.create({ name, 
-          email, 
-          password,
-        })
+        return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(password,salt))
+          .then(hash => User.create({
+            name,
+            email,
+            password: hash // 用雜湊值取代原本的使用者密碼
+          }))
         .then(()=>res.redirect('/'))
         .catch(err => console.log(err))
       }
